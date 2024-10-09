fa5305348858b6526e3fe670994a6e117fdb1e41
naetharu <naetharu@gmail.com>
2024-10-09 03:38:42 +0100

    Create toggle component

---
+++ markdown-editor-ui/src/components/Toggle/Toggle.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Toggle.module.css";
+
+interface ToggleProps {
+  leftIcon: React.ReactElement<SVGAElement>;
+  rightIcon: React.ReactElement<SVGAElement>;
+  on: boolean;
+  onClick: () => void;
+}
+
+const Toggle = ({ leftIcon, rightIcon, on, onClick }: ToggleProps) => {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      <div className={`${on ? styles.fade : ""} ${styles.center}`}>
+        {leftIcon}
+      </div>
+      <div className={styles.sliderBackground}>
+        <div
+          className={`${styles.sliderButton} ${
+            on ? styles.sliderButtonOn : styles.sliderButtonOff
+          }`}
+        />
+      </div>
+      <div className={`${on ? "" : styles.fade} ${styles.center}`}>
+        {" "}
+        {rightIcon}
+      </div>
+    </button>
+  );
+};
+
+export default Toggle;
